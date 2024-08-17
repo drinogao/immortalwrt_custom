8a0ecdae53fa502ae598ced0248ad1f1eeb42c16
Drino <qq418484008@outlook.com>
2024-08-17 17:19:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,4 @@
 
 # Add a feed source
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
- rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
