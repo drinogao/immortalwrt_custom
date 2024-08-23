cbe2e89c5760289978591d71b113043585f509c4
Drino <qq418484008@outlook.com>
2024-08-23 10:39:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,9 @@ rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,sm
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+echo "sed -i '/small/d' /etc/opkg/distfeeds.conf" >> package/base-files/files/etc/uci-defaults/14_custom-config
+echo "sed -i '/kenzo/d' /etc/opkg/distfeeds.conf" >> package/base-files/files/etc/uci-defaults/14_custom-config 
+echo "sed -i '/signature/d' opkg.conf" >> package/base-files/files/etc/uci-defaults/14_custom-config
+echo 'echo "#option check_signature" >> /etc/opkg.conf' >> package/base-files/files/etc/uci-defaults/14_custom-config 
+echo 'echo "src/gz openwrt_smpackage https://op.dllkids.xyz/packages/x86_64/" >> /etc/opkg/customfeeds.conf' >> package/base-files/files/etc/uci-defaults/14_custom-config
+echo 'exit 0' >> package/base-files/files/etc/uci-defaults/14_custom-config
